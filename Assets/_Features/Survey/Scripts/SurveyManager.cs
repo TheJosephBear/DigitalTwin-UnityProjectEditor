9d51b50d33137d740473b1e03b9ae07101f7e9d8
Joseph Bear <128801641+TheJosephBear@users.noreply.github.com>
2026-01-29 14:16:04 +0100

    cloud upload...

---
+++ Assets/_Features/Survey/Scripts/SurveyManager.cs
@@ -0,0 +1,36 @@
+using QuestionnaireToolkit.Scripts;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using QuestionnaireToolkit;
+using TMPro;
+using System;
+
+public class SurveyManager : MonoBehaviour {
+
+    public SurveyBuilder SurveyBuilderPrefab;
+    
+   
+    void Start() {
+       
+    }
+
+    public void EnterSurveyBuilding() {
+        SurveyBuilder builder = SceneLoadingManager.Instance
+            .InstantiateObjectInScene(SurveyBuilderPrefab.gameObject)
+            .GetComponent<SurveyBuilder>();
+        builder.Initialize();
+    }
+
+    public void StartSurveyRuntime() {
+
+    }
+
+
+    public void CreateNewQuestionnare() {
+       
+    }
+
+  
+}
