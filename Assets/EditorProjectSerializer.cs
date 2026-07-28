aa744b06baf46e98dde70b2e5f5b4ad59b03eda9
Pepa Bůh <128801641+TheJosephBear@users.noreply.github.com>
2026-07-28 11:55:32 +0200

    redundant messages removed
    project serialization, deserialization, successful download

--- Assets/EditorProjectSerializer.cs
+++ Assets/EditorProjectSerializer.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class EditorProjectSerializer : MonoBehaviour {
 
     public SerializableProject SerializeProject() {
-        MessageDisplayManager.Instance.DisplayMessage("Serializace projektu");
+    //    MessageDisplayManager.Instance.DisplayMessage("Serializace projektu");
         Project OpenedProject = ProjectManager.Instance.SelectedProject;
 
         SerializableProject serializableProject = new SerializableProject {
@@ -30,7 +30,7 @@ public class EditorProjectSerializer : MonoBehaviour {
     }
 
     IEnumerator DeserializeCoroutine(Project project) {
-        MessageDisplayManager.Instance.DisplayMessage("Deserializace projektu");
+    //    MessageDisplayManager.Instance.DisplayMessage("Deserializace projektu");
         UIManager.Instance.ShowUI(UIType.LoadingScreen);
 
         SerializableProject serializedProject = project.SerializableProject;
